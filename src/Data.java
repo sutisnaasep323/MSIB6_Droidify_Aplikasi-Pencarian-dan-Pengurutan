74ad30dd80107e27c3abf979d825a3fd89d24b27
Asep Sutisna <sutisnaasep323@gmail.com>
2024-03-22 10:36:45 +0700

    Add class Data, solved task B

---
+++ src/Data.java
@@ -0,0 +1,27 @@
+
+public class Data {
+    private int nilai;
+    private String deskripsi;
+
+    public Data(int nilai, String deskripsi) {
+        this.nilai = nilai;
+        this.deskripsi = deskripsi;
+    }
+
+    public int getNilai() {
+        return nilai;
+    }
+
+    public void setNilai(int nilai) {
+        this.nilai = nilai;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+}
+
